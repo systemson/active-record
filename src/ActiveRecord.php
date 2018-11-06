a01d8ac87e7021cd6845d25bf299a88cd8ff33e3
Systemson <systemson@gmail.com>
2018-11-06 15:16:48 -0400

    Updates

--- src/ActiveRecord.php
+++ src/ActiveRecord.php
@@ -2,6 +2,10 @@
 
 namespace Amber\Model;
 
+/**
+ * @todo MUST decide what architecture base this project. Active Record or Data Mapper.
+ *       DO NOT develop until the architecture is decided.
+ */
 class ActiveRecord
 {
 }
